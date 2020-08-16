2ee5216ec8f6033376e132d4979a19b6d42bbcd8
Villan-98 <sachinrathore454@gmail.com>
2020-08-16 19:14:58 +0530

    boiler-plate

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     Resource Management App
+     <button className="btn btn-primary"> Clicked</button>
+    </div>
+  );
+}
+
+export default App;
